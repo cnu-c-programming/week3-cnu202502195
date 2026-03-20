e3f8b2ec1916eb2571f0a3ace10908c39fe1ef5e
cnu202502195 <202502195@o.cnu.ac.kr>
2026-03-20 10:29:58 +0900

    1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -19,9 +19,9 @@ int div(int x, int y) {
 int main(void) {
     int a = 10, b = 5;
 
-    printf("%d + %d = %d", a, b, add(a, b));
-    printf("%d - %d = %d", a, b, sub(a, b));
-    printf("%d * %d = %d", a, b, mul(a, b));
+    printf("%d + %d = %d\n", a, b, add(a, b));
+    printf("%d - %d = %d\n", a, b, sub(a, b));
+    printf("%d * %d = %d\n", a, b, mul(a, b));
     printf("%d / %d = %d", a, b, div(a, b));
 
     return 0;
