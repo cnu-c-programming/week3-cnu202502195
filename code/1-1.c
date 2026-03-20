bb3b4840f3f323554536a570f16c2fb7298aec4b
cnu202502195 <202502195@o.cnu.ac.kr>
2026-03-20 10:01:58 +0900

    1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
+int add(int x, int y) {
+    return x + y;
+}
+
+int sub(int x, int y) {
+    return x - y;
+}
+
+int mul(int x, int y) {
+    return x * y;
+}
+
+int div(int x, int y) {
+    return x / y;
+}
+
 int main(void) {
     int a = 10, b = 5;
 
-    printf("%d + %d = %d \n", a, b, a+b);
-    printf("%d - %d = %d \n", a, b, a-b);
-    printf("%d * %d = %d \n", a, b, a*b);
-    printf("%d / %d = %d", a, b, a/b);
+    printf("%d + %d = %d", a, b, add(a, b));
+    printf("%d - %d = %d", a, b, sub(a, b));
+    printf("%d * %d = %d", a, b, mul(a, b));
+    printf("%d / %d = %d", a, b, div(a, b));
 
-    
     return 0;
 }
