06f7ffc55c6f54c7a3f6952f8fa351a5a07ec52d
cnu202502195 <202502195@o.cnu.ac.kr>
2026-03-20 09:48:17 +0900

    1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -3,10 +3,10 @@
 int main(void) {
     int a = 10, b = 5;
 
-    printf("%d + %d = %d", a, b, a+b);
-    printf("%d - %d = %d", a, b, a-b);
-    printf("%d * %d = %d", a, b, a*b);
-    printf("%d / %d = %d", a, b, a/b);
+    printf("%d + %d = %d \n", a, b, a+b);
+    printf("%d - %d = %d \n", a, b, a-b);
+    printf("%d * %d = %d \n", a, b, a*b);
+    printf("%d / %d = %d \n", a, b, a/b);
 
     
     return 0;
