0b12704692e078478ca77d272e929913b98026ce
cnu202502195 <202502195@o.cnu.ac.kr>
2026-03-20 09:49:06 +0900

    1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print() {
+    printf("Welcom to computer Programming 03 Lab!\n");
+}
+int square(int n) {
+    return n * n;
+}
+
+int main() {
+    print();
+
+    int num = 5;
+    int result = square(num);
+    printf("%d %d\n", num, result);
+
+    return 0;
+}
